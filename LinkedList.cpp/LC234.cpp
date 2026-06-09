380c202732ed262b764120debb80a53e930ccc56
Divyansh1809-sh <divyanshshukla2965@gmail.com>
2026-06-09 14:34:01 +0530

    Pallindrome LL

---
+++ LinkedList.cpp/LC234.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    bool isPalindrome(ListNode* head)
+    {
+        vector<int>v;
+        ListNode* temp=head;
+        while(temp!=NULL)
+        {
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+        vector<int>v2;
+        for(int i=v.size()-1;i>=0;i--)
+        {
+            v2.push_back(v[i]);
+        }
+        bool f=true;
+        for(int i=0;i<v.size();i++)
+        {
+            if(v[i]!=v2[i])
+            f=false;
+        }
+        return f;
+    }
+};
